5e843efb904e29da0b945ec8a108b67c0c168b7d
Renan <Renan.Olive.nasc@gmail.com>
2023-03-04 19:19:34 -0300

    Commit inicial

---
+++ ConsultMagic/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxPageScrollModule } from 'ngx-page-scroll';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxPageScrollModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
